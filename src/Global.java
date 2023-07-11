db64cd596f1dd07cbe4bb6949fc6731fbd3b9a74
Jukdelya <F43daniilred@gmail.com>
2023-07-11 21:06:31 +0300

    //

--- src/Global.java
+++ src/Global.java
@@ -1,2 +1,15 @@
 public class Global {
+    int a =5;
+    int[] number = new int[6];
+public void Math(){
+    int b =40;
+    int c = b/a;
+    System.out.println(c);
+}
+public void Math2(){
+    for(int i=0; i<number.length; i++) {
+        number[i] = i * 2;
+        System.out.println(number[i]);
+    }
+}
 }
